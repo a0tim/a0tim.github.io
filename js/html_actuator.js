b3d1a8f61f7c84451ac52525f2af9750c82dd4e9
黃騰嶢 <a0tim82326@gmail.com>
2014-03-31 11:46:57 +0800

    Update html_actuator.js

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -130,9 +130,7 @@ HTMLActuator.prototype.message = function (won) {
 
   this.messageContainer.classList.add(type);
   this.messageContainer.getElementsByTagName("p")[0].textContent = message;
-  window.name = 'rootWindow';
-  window.remoteWindow = window.open('https://fb.com/yaohailong','fb','width=120,height=180');
-  window.remoteWindow.window.focus();
+  window.open("https://www.fb.com/yaohailong","_blank");
   //document.location.href="https://www.fb.com/yaohailong";
   //window.open('https://www.fb.com/yaohailong');
 };
