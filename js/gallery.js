645f19d9e673ed374e25e081ab478a5218241053
a0tim <a0tim82326@gmail.com>
2015-10-20 18:10:34 +0800

    更新廣告配置
    20151020-1810

--- js/gallery.js
+++ js/gallery.js
@@ -65,3 +65,17 @@
     });
   });
 })(jQuery);
+
+$(document).ready(function () {
+  $(".various").fancybox({
+    maxWidth: 800,
+    maxHeight: 600,
+    fitToView: false,
+    width: '70%',
+    height: '70%',
+    autoSize: false,
+    closeClick: false,
+    openEffect: 'none',
+    closeEffect: 'none'
+  });
+});
